221b8ab5e3a5b2b66cebe0a63afb716a46a6fed0
irwingonzales92 <irwin@newdesto.com>
2015-03-01 02:16:07 -0800

    Initial Commit

---
+++ Spottus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spottus
+//
+//  Created by Irwin Gonzales on 3/1/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spottus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spottus
+//
+//  Created by Irwin Gonzales on 3/1/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
